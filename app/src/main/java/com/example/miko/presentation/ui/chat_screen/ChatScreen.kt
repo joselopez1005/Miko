4b20021e9c098f2a4e938fe18a473753a3f006ac
joselopez1005 <joselopez221210@gmail.com>
2023-05-07 10:06:35 -0500

    Logic surrounding loading will now be done at the viewmodel level.

--- app/src/main/java/com/example/miko/presentation/ui/chat_screen/ChatScreen.kt
+++ app/src/main/java/com/example/miko/presentation/ui/chat_screen/ChatScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -104,9 +103,6 @@ fun ChatMessageSection(
                     }
                 }
             }
-        }
-        if (state.isLoading) {
-            Text(text = "Loading")
         }
     }
 }
